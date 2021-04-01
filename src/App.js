7723574ae53f2984f6ecde96b8c9b22d31a1498b
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-04-01 12:41:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <div className="App">
+        <Header date = {today.toDateString()} />
+        <main>
+          <div className ={circleClass}>{this.state.counter}</div>
+          <button onClick ={this.addHandler} className ="add">Add one
+          </button>
+          <button onClick ={this.removeHandler} className ="remove">Remove one
+          </button>
+           <button onClick ={this.resetHandler} className ="reset">Reset
+          </button>
+        </main>
+        <Footer year={today.getFullYear()} />
+      </div>
+     );
+  
 }
 
 export default App;
