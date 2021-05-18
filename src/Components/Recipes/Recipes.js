411079e4ca1a391327b3f43d0645ce8cdc529837
Ankita Bhatnagar <ankita.bhatnagar@outlook.com>
2021-05-18 20:12:20 +0200

    fixing search bar

--- src/Components/Recipes/Recipes.js
+++ src/Components/Recipes/Recipes.js
@@ -5,43 +5,32 @@ import RecipeSinglePage from "./RecipeSinglePage";
 import RecipesForm from "../Recipes/RecipesForm";
 
 const Recipes = ({ recipesList }) => {
-  
-const searchRecipe = (e) => {
-  e.preventDefault();
-  let searchQuery = document.getElementById("search-bar").value;
-  fetch("https://radiant-ravine-43439.herokuapp.com/recipe/findbyname/" + searchQuery)
-    .then((response) => response.json())
-    .then((data) => {
-      recipesList = [data];
-    });
-};
+  const searchRecipe = (e) => {
+    e.preventDefault();
+    let searchQuery = document.getElementById("search-bar").value;
+    fetch(
+      "https://radiant-ravine-43439.herokuapp.com/recipe/findbyname/" +
+        searchQuery
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        recipesList = [data];
+      });
+  };
   const { path } = useRouteMatch();
-  
+
   const renderedResult = (
-    <div>
-      <div className="heading">
-        <h1>
-          <label htmlFor="search-bar">A Fascinating Flavour Experience</label>
-        </h1>
-        <form onSubmit={searchRecipe} method="get" id="search">
-          <input type="text" id="search-bar" name="searchQuery" />
-          <div className="buttons">
-            <button type="submit">Search Recipe</button>
-          </div>
-        </form>
-      </div>
-      <div className="posts">
-        {recipesList.map((a) => (
-          <RecipesCard
-            id={a.id}
-            name={a.name}
-            img={a.image}
-            desc={a.description}
-            type={a.type}
-            link={a.link}
-          />
-        ))}
-      </div>
+    <div className="posts">
+      {recipesList.map((a) => (
+        <RecipesCard
+          id={a.id}
+          name={a.name}
+          img={a.image}
+          desc={a.description}
+          type={a.type}
+          link={a.link}
+        />
+      ))}
     </div>
   );
 
